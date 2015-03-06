bb9ec130feaab24101ad87016b467d84db0878b0
Doug Miller <douglas.miller@gmail.com>
2015-03-06 00:12:20 -0500

    Add success and failure message

--- app/cards/cards.controller.js
+++ app/cards/cards.controller.js
@@ -5,40 +5,66 @@
     .module('app.controllers')
     .controller('CardsController', CardsController);
 
-  CardsController.$inject = ['$scope', 'CardGeneratorService'];
+  CardsController.$inject = ['$scope', '$timeout', 'CardGeneratorService'];
 
-  function CardsController($scope, CardGeneratorService) {
+  function CardsController($scope, $timeout, CardGeneratorService) {
     /*jshint validthis: true*/
     var vm = this;
     vm._cardService = CardGeneratorService;
+    vm._timeout = $timeout;
 
     vm._index = 0;
     vm.cards = this.generateCards();
 
+    vm.success = false;
+    vm.fail = false;
     vm.card = vm.cards[vm._index];
 
+    vm.timeout = null;
+
+    function success() {
+      vm.success = true;
+      vm.timeout = $timeout(function () {
+        vm.answer = null;
+        vm._index += 1;
+        vm.cards = vm.generateCards();
+        vm.card = vm.cards[0];
+        vm.success = false;
+      }, 1000);
+    }
+
+    function fail() {
+      vm.fail = true;
+      vm.timeout = $timeout(function () {
+        vm.fail = false;
+      }, 1000);
+    }
+
+    function validate(newValue, oldValue) {
+      if (newValue) {
+
+        var answer = vm.card.calculate().toString();
+        if (newValue.length >= answer.length) {
+          vm.success = false;
+          vm.fail = false;
+          if (vm.timeout) {
+            $timeout.cancel(vm.timeout);
+          }
+          vm.card.validate(newValue) ? success() : fail();
+        }
+      }
+    };
 
     $scope.$watch(
       function () {
         return vm.answer;
       },
-      function (newValue, oldValue) {
+      validate);
 
-        if (vm.card.validate(newValue)) {
-          vm.answer = null;
-          vm._index += 1;
-          if (vm._index >= vm.cards.length) {
-            vm._index = 0;
-          }
-          vm.cards = vm.generateCards();
-          vm.card = vm.cards[vm._index];
-        }
-
-      });
   };
 
   CardsController.prototype.generateCards = function () {
-    return this._cardService.generateCards(1, 10, '+', 3);
+    return this._cardService.generateCards(1, 10, '+', 1);
   }
 
 })();
